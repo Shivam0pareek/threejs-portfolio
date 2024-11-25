441ac58080c377d2e169b5344ead2a794cccc1cc
shivam0pareek <pareekshivam60@gmail.com>
2024-11-25 21:56:10 +0530

    new_website

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -26,9 +26,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'Shivam Pareek',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'pareekshivam045@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
